4ae29b2f78bc85f04ffd432b2564123d2f6b9a4c
Muhammad Sameer <sameertechprogrammer@gmail.com>
2022-08-06 21:43:03 +0500

    Day-3* completed

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final days = 2;
+  final days = 3;
   String name = "Muhammad sameer";
   var patanhi = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("menu"),
+      ),
       body: Center(
         child: Container(
           child: Text("wecome to $days day of flutter my name is $name "),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mynotes/Home_page.dart';
+import 'package:mynotes/Pages/Home_page.dart';
+import 'package:mynotes/Pages/Login_Page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //    home: HomePage(),
+      themeMode: ThemeMode.light, ////optioanl
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.brown),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Loginpage(),
+        "/Home": (context) => HomePage(),
+        "/login": (context) => Loginpage()
+      },
     );
   }
 }
