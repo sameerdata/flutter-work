8b2c9e75bb022765e2f4f17f282352d8bdec8de2
Muhammad Sameer <sameertechprogrammer@gmail.com>
2022-08-30 15:43:52 +0500

    Day 13 completed

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -1,16 +1,37 @@
 // ignore_for_file: file_names, must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mynotes/models/Catelog.dart';
 import 'package:mynotes/widgets/drawer.dart';
-
+import 'dart:convert';
 import '../widgets/item_widgets.dart';
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final days = 30;
+
   String name = "Muhammad sameer";
+
   var patanhi = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catelogjson = await rootBundle.loadString("assets/files/catelog.json");
+    var decodeData = jsonDecode(catelogjson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
